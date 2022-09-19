05732310ec74e8dfa5244c49c0cc9a3c4f52976b
Anthony BARRIER <abarrier@e1r9p9.clusters.42paris.fr>
2022-09-19 17:46:31 +0200

    UPDT - update ft_panic to display PROG_NAME, func, mesg
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   ft_panic.c

--- libft/message/ft_panic.c
+++ libft/message/ft_panic.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 15:33:20 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/19 09:39:32 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/19 17:45:55 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ int	ft_panic(int errcode, const char *filename, char *msg)
 		err_msg = msg;
 	else
 		err_msg = NULL;
-	res += ft_putendl_fd_const(err_msg, 2);
+	res += ft_putstr_fd_const("cub3D", 2);
+	res += ft_putstr_fd_const(": ", 2);
 	if (filename)
 	{
-		res += ft_putstr_fd_const("func ->  ", 2);
-		res += ft_putendl_fd_const(filename, 2);
+		res += ft_putstr_fd_const(filename, 2);
+		res += ft_putstr_fd_const(": ", 2);
 	}
+	res += ft_putendl_fd_const(err_msg, 2);
 	return (res);
 }
