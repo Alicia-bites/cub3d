31af1082d75ea21bc32e52e10a89673094ded13e
Anthony BARRIER <abarrier@e1r1p7.clusters.42paris.fr>
2022-09-20 17:03:41 +0200

    FIX - fix wrong error message if error on opening file
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/parsing/parse.c

--- sources/parsing/parse.c
+++ sources/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 09:26:46 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/19 18:20:07 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:03:02 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ int	parse(int argc, char **argv, t_mlx *mlx)
 {
 	if (parse_arg(argc))
 		return (EXIT_FAILURE);
-	if (parse_map_fd(argv[1], mlx) < 0)
+	if (parse_map_fd(argv[1], mlx) != 0)
 		return (EXIT_FAILURE);
 	parse_read_file(mlx);
+	close(mlx->map_fd);
+	mlx->map_fd = FD_NOT_INIT;
 	if (mlx->map_fd_lst == NULL)
 		return (EXIT_FAILURE);
 	if (ft_lst_size(mlx->map_fd_lst) < MAP_FILE_MIN_LINE)
 		return (ft_panic_value(-1, __func__, ERR_MAP_CONTENT,
 				EXIT_FAILURE));
+//	parse_read_lst(mlx)
 	return (0);
 }
