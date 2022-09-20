4941fde5aaffa71f2accd7e219fe239d784ea8a0
Anthony BARRIER <abarrier@e1r1p7.clusters.42paris.fr>
2022-09-20 12:29:45 +0200

    UPDT - change comparison from ft_strcmp to enum comparison
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/settings/settings_texture_fd_check.c
    	modified:   sources/settings/settings_texture_fd_save.c

--- sources/settings/settings_texture_fd_check.c
+++ sources/settings/settings_texture_fd_check.c
@@ -6,30 +6,26 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 15:56:07 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/19 18:22:56 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:46:20 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-int	settings_texture_fd_check(t_mlx *mlx, char *text_type)
+int	settings_texture_fd_check(t_mlx *mlx, int txt_type)
 {
 	t_settings	*settings;
 	int			res;
 
 	settings = &mlx->settings;
 	res = 0;
-	if (ft_strcmp(text_type, MAP_TEXT_NO) == 0
-		&& settings->fd_no != FD_NOT_INIT)
+	if (txt_type == NO && settings->fd_no != FD_NOT_INIT)
 		res = EXIT_FAILURE;
-	else if (ft_strcmp(text_type, MAP_TEXT_SO) == 0
-		&& settings->fd_so != FD_NOT_INIT)
+	else if (txt_type == SO && settings->fd_so != FD_NOT_INIT)
 		res = EXIT_FAILURE;
-	else if (ft_strcmp(text_type, MAP_TEXT_WE) == 0
-		&& settings->fd_we != FD_NOT_INIT)
+	else if (txt_type == WE && settings->fd_we != FD_NOT_INIT)
 		res = EXIT_FAILURE;
-	else if (ft_strcmp(text_type, MAP_TEXT_EA) == 0
-		&& settings->fd_ea != FD_NOT_INIT)
+	else if (txt_type == EA && settings->fd_ea != FD_NOT_INIT)
 		res = EXIT_FAILURE;
 	if (res)
 		ft_panic(-1, __func__, ERR_MAP_DUP_TEXT);
--- sources/settings/settings_texture_fd_save.c
+++ sources/settings/settings_texture_fd_save.c
@@ -6,24 +6,24 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 15:56:07 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/19 17:32:11 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:39:29 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-int	settings_texture_fd_save(t_mlx *mlx, char *text_type, int fd)
+int	settings_texture_fd_save(t_mlx *mlx, int txt_type, int fd)
 {
 	t_settings	*settings;
 
 	settings = &mlx->settings;
-	if (ft_strcmp(text_type, MAP_TEXT_NO) == 0)
+	if (txt_type == NO)
 		settings->fd_no = fd;
-	else if (ft_strcmp(text_type, MAP_TEXT_SO) == 0)
+	else if (txt_type == SO)
 		settings->fd_so = fd;
-	else if (ft_strcmp(text_type, MAP_TEXT_WE) == 0)
+	else if (txt_type == WE)
 		settings->fd_we = fd;
-	else if (ft_strcmp(text_type, MAP_TEXT_EA) == 0)
+	else if (txt_type == EA)
 		settings->fd_ea = fd;
 	else
 		return (EXIT_FAILURE);
