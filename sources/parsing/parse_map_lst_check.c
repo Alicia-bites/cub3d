02188821aaf9dd881b5016aa522c8d97aaaaf03f
Anthony BARRIER <abarrier@e1r3p6.clusters.42paris.fr>
2022-09-21 16:47:55 +0200

    NORM -
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   parse_map_lst_check.c

--- sources/parsing/parse_map_lst_check.c
+++ sources/parsing/parse_map_lst_check.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 12:58:38 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/21 13:11:59 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:47:34 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	parse_map_lst_check(t_mlx *mlx, int txt_type, char *value)
 {
-	if ((txt_type == NO || txt_type == SO || txt_type == WE
-		|| txt_type == EA)
+	if ((txt_type == NO || txt_type == SO
+			|| txt_type == WE || txt_type == EA)
 		&& settings_texture_fd_open(mlx, txt_type, value) != 0)
 		return (EXIT_FAILURE);
 	else if ((txt_type == F || txt_type == C)
