43c6f90bcb4794bffed9c22da4b5435ab1803725
Anthony BARRIER <abarrier@e1r3p6.clusters.42paris.fr>
2022-09-21 19:02:56 +0200

    UPDT - change sequence to check if duplicate and save line_no into the dedicated property
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/parsing/parse_map_lst_check.c

--- sources/parsing/parse_map_lst_check.c
+++ sources/parsing/parse_map_lst_check.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 12:58:38 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/21 17:57:21 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/21 18:35:26 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 int	parse_map_lst_check(t_mlx *mlx, int txt_type, t_map_fd *map,
 		char *value)
 {
-	if ((txt_type == NO || txt_type == SO
+	if (txt_type == NO || txt_type == SO
 			|| txt_type == WE || txt_type == EA)
-		&& settings_texture_fd_open(mlx, txt_type, value) != 0)
-		return (EXIT_FAILURE);
-	else if ((txt_type == F || txt_type == C)
-		&& settings_rgb(mlx, txt_type, value) != 0)
-		return (EXIT_FAILURE);
-	else if (txt_type == MAP && mlx->settings.map_start_line < 0)
-		mlx->settings.map_start_line = map->line_no;
+	{
+		if (settings_texture_fd_open(mlx, txt_type, value) != 0)
+			return (EXIT_FAILURE);
+		settings_texture_fd_set_line_no(mlx, txt_type, map);
+	}
+	else if (txt_type == F || txt_type == C)
+	{
+		if (settings_rgb(mlx, txt_type, value) != 0)
+			return (EXIT_FAILURE);
+		settings_rgb_set_line_no(mlx, txt_type, map);
+	}
+	else if (txt_type == MAP && mlx->settings.map_start_line_no < 0)
+		mlx->settings.map_start_line_no = map->line_no;
 	return (0);
 }
