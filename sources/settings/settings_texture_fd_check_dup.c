0ebfe5c089122470435b4c7016b1d4b266027879
Anthony BARRIER <abarrier@e1r3p6.clusters.42paris.fr>
2022-09-21 12:43:38 +0200

    FIX - fix typo
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   settings_texture_fd_check_dup.c

--- sources/settings/settings_texture_fd_check_dup.c
+++ sources/settings/settings_texture_fd_check_dup.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 15:56:07 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/21 12:37:39 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/21 12:40:06 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	settings_texture_fd_check_dup(t_mlx *mlx, int txt_type)
 {
 	t_settings	*settings;
 
-	settings = &mlx-settings;
+	settings = &mlx->settings;
 	if (txt_type == NO && settings->fd_no >= 0)
 		return (ft_panic(-1, __func__, ERR_MAP_DUP_TEXT));
 	else if (txt_type == SO && settings->fd_so >= 0)
