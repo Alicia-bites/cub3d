b64edc681efbae5275e7d999eae6736156b5af95
Anthony BARRIER <abarrier@e1r12p13.clusters.42paris.fr>
2022-09-23 17:33:02 +0200

    FIX - fix error of printing map_width without initialize the value
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   settings_init_map.c

--- sources/settings/settings_init_map.c
+++ sources/settings/settings_init_map.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 11:00:59 by abarrier          #+#    #+#             */
-/*   Updated: 2022/09/23 11:01:50 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/09/23 17:31:28 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	settings_init_map(t_settings *settings)
 	settings->map_start_line_no = -1;
 	settings->map_end_line_no = -1;
 	settings->map_width = -1;
+	settings->map_height = -1;
 	settings->map_player_sp_val = -1;
 	settings->map_player_sp_x = -1;
 	settings->map_player_sp_y = -1;
