b0897a31bced5fcc5d6de89a3e2e6643c5b0028e
Alicia MARCHAND <amarchan@e1r12p14.clusters.42paris.fr>
2022-09-27 18:04:23 +0200

    ADD - IN PROCESS - getting parsing map's in the code
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/ray_casting/perform_dda.c

--- sources/ray_casting/perform_dda.c
+++ sources/ray_casting/perform_dda.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 18:09:58 by amarchan          #+#    #+#             */
-/*   Updated: 2022/09/27 17:29:08 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/09/27 17:57:31 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,17 @@ int	perform_dda(t_game *game)
 			game->side = 1;
 		}
 		// Check if ray has hit a wall
-		if (worldMap[game->mapX][game->mapY] > 0)
-			game->hit = 1;
-		// print_tab(game->map_tab, game->settings.map_width, game->settings.map_height);
-		// printf("game->map_tab = %p\n", game->map_tab);
-		// printf("game->mapX = %d\n", game->mapX);
-		// printf("game->mapY = %d\n", game->mapY);
-		// printf("game->settings->map_height = %d\n", game->settings.map_height);
-		// printf("game->settings->map_width = %d\n", game->settings.map_width);
-		// printf("game->map_tab[game->mapX][game->mapY] = %d\n", game->map_tab[game->mapX][game->mapY]);
-		// if (game->map_tab[game->mapX][game->mapY] > 48)
+		// if (worldMap[game->mapX][game->mapY] > 0)
 		// 	game->hit = 1;
+		print_tab(game->map_tab, game->settings.map_width, game->settings.map_height);
+		// printf("game->map_tab = %p\n", game->map_tab);
+		printf("game->mapX = %d\n", game->mapX);
+		printf("game->mapY = %d\n", game->mapY);
+		printf("game->settings->map_height = %d\n", game->settings.map_height);
+		printf("game->settings->map_width = %d\n", game->settings.map_width);
+		printf("game->map_tab[game->mapX][game->mapY] = %d\n", game->map_tab[game->mapX][game->mapY]);
+		if (game->map_tab[game->mapX][game->mapY] > 48)
+			game->hit = 1;
 	}
 	return (0);
 }
