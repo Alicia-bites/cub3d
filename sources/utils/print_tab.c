927304af4df4a6ca3d50b034c7c4f0ee0077876f
Alicia MARCHAND <amarchan@e2r11p4.clusters.42paris.fr>
2022-09-29 10:50:01 +0200

    ADD - colors to the printing of the map. Yes, it's useless but I had fun. And now it's pretty.
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/utils/print_tab.c

--- sources/utils/print_tab.c
+++ sources/utils/print_tab.c
@@ -6,11 +6,36 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:09:09 by amarchan          #+#    #+#             */
-/*   Updated: 2022/09/28 16:09:17 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/09/29 10:49:45 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"cub.h"
+
+void	cyan()
+{
+	printf("\033[0;36m");
+}
+
+void	green()
+{
+	printf("\033[0;32m");
+}
+
+void	blue()
+{
+	printf("\033[0;34m");
+}
+
+void	purple()
+{
+	printf("\033[1;35m");
+}
+
+void	reset ()
+{
+	printf("\033[0m");
+}
 
 //FOR DEBUG
 void	print_tab(int **worldMap, int map_width, int map_height)
@@ -25,10 +50,17 @@ void	print_tab(int **worldMap, int map_width, int map_height)
 		printf(("{"));
 		while (j < map_width)
 		{
+			if (worldMap[i][j] == 1)
+				cyan();
+			else if (worldMap[i][j] == 0)
+				green();
+			else if (worldMap[i][j] > 1)
+				purple();
 			printf("%d,", worldMap[i][j]);
 			j++;
 		}
 		i++;
+		reset();
 		printf(("}"));
 		printf(("\n"));
 	}
