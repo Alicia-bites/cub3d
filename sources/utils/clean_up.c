f13d2489d93bf90c27c5dcff290d7c37be1890c7
Alicia MARCHAND <amarchan@e2r11p4.clusters.42paris.fr>
2022-09-29 11:27:21 +0200

    ADD - function to clean up mlx stuff
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/utils/clean_up.c

--- sources/utils/clean_up.c
+++ sources/utils/clean_up.c
@@ -6,11 +6,19 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:16:50 by amarchan          #+#    #+#             */
-/*   Updated: 2022/09/28 15:22:41 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/09/29 11:26:14 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+void	free_mlx(t_game *game)
+{
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+}
 
 static void	free_texture_tab(int **texture)
 {
@@ -27,5 +35,6 @@ static void	free_texture_tab(int **texture)
 
 void	clean_up(t_game *game)
 {
+	free_mlx(game);
 	free_texture_tab(game->texture);
 }
