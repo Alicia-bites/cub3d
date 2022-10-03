5425ca0eb7aa49fc87e5f80f0c914f3a6e9450d0
Alicia MARCHAND <amarchan@e2r9p4.clusters.42paris.fr>
2022-10-03 12:25:50 +0200

    FIX - STARTING FACING THE RIGHT DIRECTION!!!
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   sources/ray_casting/init_struct.c

--- sources/ray_casting/init_struct.c
+++ sources/ray_casting/init_struct.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 18:11:05 by amarchan          #+#    #+#             */
-/*   Updated: 2022/10/03 12:08:26 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/10/03 12:23:58 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,34 @@
 // Check if player is facing North, South, East or West
 void	init_starting_direction(t_game *game)
 {
-	printf("setup.map_player_sp_val = %c\n", setup.map_player_sp_val);
 	if (setup.map_player_sp_val == 'N')
 	{
 		game->dirX = -1.0;
 		game->dirY = 0.0;
-		game->planeX = 0.0;
-		game->planeY = -0.66;
+		game->planeX = 0.0 * game->dirY;
+		game->planeY = -0.66 * game->dirX;
 
 	}
 	else if (setup.map_player_sp_val == 'S')
 	{
 		game->dirX = 1.0;
 		game->dirY = 0.0;
-		game->planeX = 0.0;
-		game->planeY = 0.66;
+		game->planeX = 0.0 * game->dirY;
+		game->planeY = 0.66 * -game->dirX;
 	}
 	else if (setup.map_player_sp_val == 'E')
 	{
 		game->dirX = 0.0;
 		game->dirY = 1.0;
-		game->planeX = 0.0;
-		game->planeY = 0.66;
+		game->planeX = 0.66 * game->dirY;
+		game->planeY = 0.0 * game->dirX;
 	}
 	else if (setup.map_player_sp_val == 'W')
 	{
 		game->dirX = 0.0;
 		game->dirY = -1.0;
-		game->planeX = 0.0;
-		game->planeY = 0.66;
+		game->planeX = 0.66 * game->dirY;
+		game->planeY = 0.0 * game->dirX;
 	}
 }
 
@@ -51,11 +50,7 @@ int	init_struct(t_game *game)
 {
 	game->posX = setup.map_player_sp_y;
 	game->posY = setup.map_player_sp_x;
-	game->dirX = -1.0;
-	game->dirY = 0.0;
-	game->planeX = 0.0;
-	game->planeY = 0.66;
-	// init_starting_direction(game);
+	init_starting_direction(game);
 	game->re_buf = 0;
 	return (0);
 }
